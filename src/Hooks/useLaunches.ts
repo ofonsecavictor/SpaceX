de9070a656f9c5c5699f9760f0e49bf419607138
Victor Fonseca <contatovictorfonseca@gmail.com>
2022-08-03 17:16:16 -0300

    remove desnecessary get

--- src/Hooks/useLaunches.ts
+++ src/Hooks/useLaunches.ts
@@ -3,56 +3,6 @@ import { useQuery } from 'react-query';
 import { spaceX } from '../api/client.tsx';
 import type { LaunchPaginationProps, LaunchProps } from './lauches';
 
-const getUpcomingLaunches = async (): Promise<LaunchProps[]> => {
-    const { data } = await spaceX.post<LaunchPaginationProps>(
-        '/launches/query',
-        {
-            query: {
-                upcoming: true,
-            },
-            options: {
-                pagination: false,
-                sort: {
-                    flight_number: 'asc',
-                },
-                populate: [
-                    'payloads',
-                    'rocket',
-                    'launchpad',
-                    {
-                        path: 'cores',
-                        populate: [
-                            {
-                                path: 'landpad',
-                            },
-                            {
-                                path: 'core',
-                            },
-                        ],
-                    },
-                ],
-            },
-        },
-    );
-
-    const launchOrder = ['hour', 'day', 'month', 'year', 'quarter', 'half'];
-
-    return data.docs.sort((a, b) => {
-        if (a.date_precision === 'hour' && b.date_precision === 'day') {
-            return a.date_unix - b.date_unix;
-        }
-
-        return (
-            launchOrder.indexOf(a.date_precision) -
-            launchOrder.indexOf(b.date_precision)
-        );
-    });
-};
-
-export function useUpcomingLaunches() {
-    return useQuery(['upcomingLaunches'], getUpcomingLaunches);
-}
-
 const getRecentLaunches = async (): Promise<LaunchProps[]> => {
     const { data } = await spaceX.post<LaunchPaginationProps>(
         '/launches/query',
